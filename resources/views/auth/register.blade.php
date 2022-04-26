f2f0ec2def434ed6016b0e8b7b67cf6fa3b962f7
Angel Eduardo <alvaza132@gmail.com>
2022-04-26 16:54:52 +0200

    Register-Select-mejor

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,30 +7,41 @@
         <x-jet-validation-errors class="mb-4" />
 
  
-        <form method="POST" action="{{ route('register') }}"  @if (old('role_id')==3 ) x-data="{role_id: 3}"  @endif    x-data="{role_id: 2}">
+        <form method="POST" action="{{ route('register') }}"  @if (old('role_id')==3 ) x-data="{role_id: 3}"  @elseif (old('role_id')==2 ) x-data="{role_id: 2}"    @endif    x-data="{role_id: 0}">
             @csrf
 
-            <div>
+            <div class="mt-4">
+                <x-jet-label for="role_id" value="{{ __('Register as:') }}" />
+                <select  name="role_id" x-model="role_id" class="block mt-1 w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
+                <option value="0">Registrarse como:</option>
+                <option value="2">Cliente</option>
+                <option value="3">Abogado</option>
+                </select>
+            </div>
+
+
+
+            <div x-show="role_id == 2 || role_id == 3 ">
                 <x-jet-label for="name" value="{{ __('Name') }}" />
                 <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             </div>
 
-            <div class="mt-4">
+            <div class="mt-4" x-show="role_id == 2 || role_id == 3">
                 <x-jet-label for="email" value="{{ __('Email') }}" />
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
+            </div x-show="role_id == 2">
 
-            <div class="mt-4">
+            <div class="mt-4" x-show="role_id == 2  || role_id == 3">
                 <x-jet-label for="password" value="{{ __('Password') }}" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
             </div>
 
-            <div class="mt-4">
+            <div class="mt-4" x-show="role_id == 2  || role_id == 3">
                 <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
                 <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
             </div>
 
-            <div class="mt-4">
+            <div class="mt-4" x-show="role_id == 2  || role_id == 3">
                 <x-jet-label for="gender_id" value="{{ __('Sexo') }}" />
                 <select name="gender_id" id="gender_id" class="block mt-1 w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
                     @foreach ($gender as $gender )
@@ -38,14 +49,6 @@
                     @endforeach
                 </select>
             </div>
-            <div class="mt-4">
-                <x-jet-label for="role_id" value="{{ __('Register as:') }}" />
-                <select  name="role_id" x-model="role_id" class="block mt-1 w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
-                <option value="2">Cliente</option>
-                <option value="3">Abogado</option>
-                </select>
-            </div>
-
 
         
             <div class="mt-4" x-show="role_id == 2">
@@ -134,15 +137,27 @@
                 </div>
             @endif
 
-            <div class="flex items-center justify-end mt-4">
+  
+          
+            <div class="flex items-center justify-end mt-4" >
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
                     {{ __('Already registered?') }}
                 </a>
 
-                <x-jet-button class="ml-4">
+                <x-jet-button class="ml-4" x-show="role_id == 2  || role_id == 3" >
                     {{ __('Register') }}
                 </x-jet-button>
+
+                <a class="ml-4 underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('welcome') }}" x-show="role_id == 0" >
+                    {{ __('Volver') }}
+                </a>
+
+                
             </div>
+
+     
+
+   
         </form>
     </x-jet-authentication-card>
 </x-guest-layout>
