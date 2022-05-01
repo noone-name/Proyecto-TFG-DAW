fa27b5e27bcaa7ebafe746425ddcc451bd0991b9
Angel Eduardo <alvaza132@gmail.com>
2022-05-01 16:03:03 +0200

    --sd

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\Admin\UserController;
+use \App\Http\Controllers\Clients\ListingController;
+use \App\Http\Controllers\Lawyer\ManageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +22,17 @@ Route::get('/', function () {
 
 Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])->group(function () {
     Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
+});
 
 
+Route::group(['middleware' => 'auth'], function() {
+    Route::group(['middleware' => 'role:client', 'prefix' => 'client', 'as' => 'client.'], function() {
+        Route::resource('lessons', ListingController::class);
+    });
+   Route::group(['middleware' => 'role:lawyer', 'prefix' => 'lawyer', 'as' => 'lawyer.'], function() {
+       Route::resource('courses', ManageController::class);
+   });
+    Route::group(['middleware' => 'role:admin', 'prefix' => 'admin', 'as' => 'admin.'], function() {
+        Route::resource('users', UserController::class);
+    });
 });
