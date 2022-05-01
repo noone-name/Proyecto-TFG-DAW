11728415691bd681fd87a372ac3e8648dd373277
Angel Eduardo <alvaza132@gmail.com>
2022-05-01 20:30:25 +0200

    SuperAdmin Manager

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(RolesAndPermissionsSeeder::class);
         $this->call(GendersTableSeeder::class);
+        $this->call(AdminsSeeder::class);
+
+        
     }
 }
--- resources/views/client/listings/index.blade.php
+++ resources/views/client/listings/index.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-client-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Courses List') }}
@@ -10,4 +10,4 @@
             Accessible only for clients.
         </div>
     </div>
-</x-app-layout>
+</x-client-layout>
