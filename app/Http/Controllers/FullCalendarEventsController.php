7227e032d5122b6d02128ed5890b86330ac7de00
Angel Eduardo <alvaza132@gmail.com>
2022-05-05 20:56:17 +0200

    Crear make:model FullCalendarEvents mcr

---
+++ app/Http/Controllers/FullCalendarEventsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FullCalendarEvents;
+use Illuminate\Http\Request;
+
+class FullCalendarEventsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\FullCalendarEvents  $fullCalendarEvents
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FullCalendarEvents $fullCalendarEvents)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\FullCalendarEvents  $fullCalendarEvents
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FullCalendarEvents $fullCalendarEvents)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\FullCalendarEvents  $fullCalendarEvents
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FullCalendarEvents $fullCalendarEvents)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\FullCalendarEvents  $fullCalendarEvents
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FullCalendarEvents $fullCalendarEvents)
+    {
+        //
+    }
+}
