2d8cfce8992b09ce73a873788627eeecb6c27723
Angel Eduardo <alvaza132@gmail.com>
2022-05-07 16:56:25 +0200

    RemoveDatapicker

--- resources/views/layouts/lawyer.blade.php
+++ resources/views/layouts/lawyer.blade.php
@@ -23,7 +23,6 @@
 
 
     <script src="https://unpkg.com/flowbite@1.4.5/dist/flowbite.js"></script>
-    <script src="https://unpkg.com/flowbite@1.4.5/dist/datepicker.js"></script>
 
 
     <script src="{{ asset('js/jquery-3.6.0.min.js') }}" defer></script>
